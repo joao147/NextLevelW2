34e3a8eae5053c2c4ccf7e7844981a55bec3f5aa
unknown <j218927@dac.unicamp.br>
2020-08-03 21:00:34 -0400

    Oi

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import logoImg from '../../assets/img/logo.svg'
+import landingImg from '../../assets/img/landing.svg'
 
 function Landing(){
-    return(
-        <h1>
-            é nois;
-        </h1>
-    )
+  return(
+    <div id="page-landing">
+      <div id="page-landing-content" className="container">
+
+        <div className="logo-container">
+          <img src={logoImg} alt="Proffy"/>
+          <h2>Sua plataforma de estudos online.</h2>
+        </div>
+
+        <img 
+          src={landingImg}
+          alt="Plataforma de estudos"
+          className="hero-image"
+        />
+
+      </div>
+    </div>
+  )
 }
 
 export default Landing;
